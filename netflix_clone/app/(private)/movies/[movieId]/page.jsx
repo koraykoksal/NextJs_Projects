1fa1940933a16ff66d7038c9fb64e0d6b6d9d6c6
koraykoksal <kksl.koray@gmail.com>
2023-08-31 17:29:06 +0300

    navbar

---
+++ netflix_clone/app/(private)/movies/[movieId]/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+//? dinamik routing yapısında id ile çağırılan datalara params propu ile karşılanır
+const MovieDetail = ({params}) => {
+
+  return (
+    <div>MovieDetail</div>
+  )
+}
+
+export default MovieDetail
